04b6bdb339d3a4ef141962be2d5f316722a28e83
Jose Damico <jose.damico@scicrop.com>
2022-11-05 20:56:34 -0300

    wip

--- src/main/java/org/jdamico/gpsd/client/threads/VerifierThread.java
+++ src/main/java/org/jdamico/gpsd/client/threads/VerifierThread.java
@@ -18,10 +18,9 @@ public class VerifierThread extends Thread {
 		while(GpsdClientRuntime.shouldListenOutput) {
 			try {
 				if(GpsdClientRuntime.outputMessageMap.containsKey("WATCH") && GpsdClientRuntime.outputMessageMap.containsKey("TPV") && GpsdClientRuntime.outputMessageMap.containsKey("WATCH") && GpsdClientRuntime.outputMessageMap.containsKey("DEVICES") && GpsdClientRuntime.outputMessageMap.containsKey("SKY")) {
-					System.out.println(GpsdClientRuntime.outputMessageMap.get("SKY"));
 					gpsSkyEntity = gson.fromJson(GpsdClientRuntime.outputMessageMap.get("SKY"), GpsSkyEntity.class);
 					if(gpsSkyEntity.getGdop() !=null && gpsSkyEntity.getGdop() < GpsdClientRuntime.DOP_MINIMAL_PRECISION && gpsSkyEntity.getPdop() !=null && gpsSkyEntity.getPdop() < GpsdClientRuntime.DOP_MINIMAL_PRECISION) {
-						
+						System.out.println(GpsdClientRuntime.outputMessageMap.get("TPV"));
 						GpsTpvEntity gpsGstEntity = gson.fromJson(GpsdClientRuntime.outputMessageMap.get("TPV"), GpsTpvEntity.class);
 						X = gpsGstEntity.getLon();
 						Y = gpsGstEntity.getLat();
